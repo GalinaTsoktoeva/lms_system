20ad3119dd803c91b99cd93bdb1cf330730ac663
gtsoktoeva001 <galina-tsoktoeva@mail.ru>
2023-08-05 12:36:18 +0300

    init project

--- users/models.py
+++ users/models.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
+
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+    email_verify = models.BooleanField(default=False)
+
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    city = models.CharField(max_length=100, verbose_name='страна', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
