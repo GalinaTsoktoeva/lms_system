4485b7dc44ab5647f5f4edabca3d0ca2b2724f72
gtsoktoeva001 <galina-tsoktoeva@mail.ru>
2023-08-14 19:18:55 +0300

    25.2 testcase done

--- course/tests.py
+++ course/tests.py
@@ -66,3 +66,64 @@ class CourseTestCase(APITestCase):
             response.json().get("results"),
             [{'id': 1, 'name': 'test', 'img': None,'link_video': 'https://www.youtube.com','course': 1, 'owner':1, 'description': 'test description', 'payment': [], 'last_payment': 0}]
         )
+
+    def test_lesson_retrieve(self) -> None:
+        self.client.force_authenticate(user=self.user)
+
+        response = self.client.get(f'/lesson/{self.lesson.pk}/')
+
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK,
+        )
+        response = response.json()
+
+        self.assertEqual(response.get('name'), 'test')
+        self.assertEqual(response.get('description'), 'test description')
+        self.assertEqual(response.get('img'), None)
+        self.assertEqual(response.get('link_video'), 'https://www.youtube.com')
+        self.assertEqual(response.get('course'), self.course.pk)
+        self.assertEqual(response.get('owner'), self.user.pk)
+
+    def test_lesson_update(self):
+        self.client.force_authenticate(user=self.user)
+
+        data = {
+            'name': 'updated test',
+            'description': 'updated test description',
+        }
+
+        response = self.client.put(
+            path=f'/lesson/update/{self.lesson.pk}/', data=data,
+        )
+
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK,
+        )
+        response = response.json()
+
+        self.assertEqual(response.get('name'), 'updated test')
+        self.assertEqual(response.get('description'), 'updated test description')
+        self.assertEqual(response.get('img'), None)
+        self.assertEqual(response.get('link_video'), 'https://www.youtube.com')
+        self.assertEqual(response.get('course'), self.course.pk)
+        self.assertEqual(response.get('owner'), self.user.pk)
+
+    def test_lesson_delete(self):
+        self.client.force_authenticate(user=self.user)
+
+        response = self.client.delete(
+            f'/lesson/delete/{self.lesson.pk}/',
+        )
+
+        self.assertEqual(
+            response.status_code, status.HTTP_204_NO_CONTENT,
+        )
+        self.assertFalse(
+            Lesson.objects.all().exists(),
+        )
+
+    def tearDown(self) -> None:
+        self.user.delete()
+        self.course.delete()
+        self.lesson.delete()
+
